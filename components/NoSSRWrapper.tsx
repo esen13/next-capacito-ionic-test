640688ac1f1a37a06fa88b307fddbfbcda4726b5
Esen13 <esen.arykbaev@gmail.com>
2023-12-26 16:49:45 +0600

    updated pods

--- components/NoSSRWrapper.tsx
+++ components/NoSSRWrapper.tsx
@@ -1,8 +1,6 @@
 import dynamic from 'next/dynamic'
 
-const NoSSRWrapper = (props: { children: React.ReactNode }) => (
-	<>{props.children}</>
-)
+const NoSSRWrapper = (props: { children: any }) => <>{props.children}</>
 
 export default dynamic(() => Promise.resolve(NoSSRWrapper), {
 	ssr: false,
--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const withTM = require('next-transpile-modules')([
-    '@ionic/react',
-    '@ionic/core',
-    '@stencil/core',
-    'ionicons'
-]);
+	'@ionic/react',
+	'@ionic/core',
+	'@stencil/core',
+	'ionicons',
+])
 
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export'
+	reactStrictMode: true,
+	output: 'export',
+	images: {
+		loader: 'custom',
+		loaderFile: './my-loader.ts',
+	},
 }
 
 module.exports = withTM(nextConfig)
